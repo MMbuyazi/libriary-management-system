8a5e9d56e0f469fd4023074a5f37f278974e5df2
MA Mbuyazi <136698187+MMbuyazi@users.noreply.github.com>
2024-03-23 23:50:51 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -2,15 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-file_path = "C:\Users\cash\DevelopmentProject\Task3\motor_data11-14lats.csv"
+file_path =pd.read_csv ("C:\\Users\\cash\\libriary-management-system\\DevelopmentProject\\motor_data11-14lats.csv")
+print(file_path.head(10))
+print(file_path[file_path['SEATS_NUM']>40][['MAKE','USAGE']])
 
-insurance_df = pd.read_csv(file_path)
+file_path['EFFECTIVE_YR'] = pd.to_numeric(file_path['EFFECTIVE_YR'], errors='coerce')
 
 
-plt.figure(figsize=(8, 6))
-plt.scatter(insurance_df['carring_capacity'], insurance_df['effective_yr'], alpha=0.5)
-plt.xlabel('Carring Capacity')
-plt.ylabel('Effective Year')
-plt.title('Carring Capacity vs. Effective Year')
-plt.grid(True)
+plt.plot(file_path['CARRYING_CAPACITY'], file_path['EFFECTIVE_YR'])
+plt.xlabel('CARRYING CAPACITY')
+plt.ylabel('EFFECTIVE YEAR')
+plt.title('Carring Capacity vs Effective Year')
 plt.show()
+
